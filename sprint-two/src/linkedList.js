2333fe98612abb67ebe50217a3cdfd08c8e2719e
josephlin55555 <josephlin55555@gmail.com>
2015-03-26 16:13:33 -0700

    Finished up until tree.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,41 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    // for an empty list, point both head and tail to the first instance of Node
+    if (list.head === null) {
+      var linkedListNode = Node(value);
+      list.head = linkedListNode;
+      list.tail = linkedListNode;
+    } else {
+      // for a non-empty list, point tail to the new instance of Node
+      var linkedListNode = Node(value);
+      list.tail.next = linkedListNode;
+      list.tail = linkedListNode;
+    }
   };
 
-  list.removeHead = function(){
+  list.removeHead = function() {
+    var removed = list.head.value;
+    list.head = list.head.next;
+    return removed;
   };
 
   list.contains = function(target){
+  //check if target is in the list
+    var tempNode = list.head;
+    var result = false;
+      var checkNode = function(node) {
+        if(tempNode.value === target) {
+          result = true;
+        }
+        if(tempNode.next !== null) {
+          tempNode = tempNode.next;
+          checkNode(tempNode);
+        }
+      };
+    checkNode(tempNode);
+    return result;
   };
-
   return list;
 };
 
@@ -21,7 +48,7 @@ var Node = function(value){
   node.value = value;
   node.next = null;
 
-  return node;
+  return node; //functional instantiation
 };
 
 /*
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,11 @@
 var Tree = function(value){
-  var newTree = {};
+  var newTree = _.extend({}, treeMethods);
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  // your code here]
+
+
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -15,11 +17,27 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var childTree = Tree(value);
+  this.children.push(childTree);
 
 };
 
 treeMethods.contains = function(target){
+  var results = false;
 
+  var checkNode = function(node) {
+
+    if(node.value === target) {
+      results = true;
+    }
+
+    for(var i = 0; i < node.children.length; i++) {
+      checkNode(node.children[i]);
+    }
+  };
+
+  checkNode(this);
+  return results;
 };
 
 
