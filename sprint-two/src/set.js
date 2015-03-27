f8768746b226d0400635742cb84420daaa29d60c
josephlin55555 <josephlin55555@gmail.com>
2015-03-27 16:35:25 -0700

    completed set from data-structures sprint-2

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item){
+  var contain = false;
+  if(this._storage.indexOf(item) !== -1) {
+    contain = true;
+  }
+  return contain;
 };
 
 setPrototype.remove = function(item){
+  if(this.contains(item)) {
+    this._storage.splice(this._storage.indexOf(item), 1);
+  }
 };
 
 /*
