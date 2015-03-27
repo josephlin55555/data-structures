73a2bcd6a7ceff672418d290daad467da47a92d0
josephlin55555 <josephlin55555@gmail.com>
2015-03-27 16:20:34 -0700

    completed graph in data-structures sprint-2

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,28 +1,67 @@
-
-
 var Graph = function(){
+
+  this.storage = {};
 };
 
 Graph.prototype.addNode = function(node){
+  this.storage[node] = new GraphNode();
 };
 
 Graph.prototype.contains = function(node){
+  var exist = false;
+  if(this.storage[node] !== undefined) {
+    exist = true;
+  }
+  return exist;
+
 };
 
 Graph.prototype.removeNode = function(node){
+  if(this.storage[node] !== undefined) {
+    delete this.storage[node];
+  }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  var containsEdge = false;
+  if(this.storage[fromNode].edges.indexOf(this.storage[toNode]) !== -1){
+    containsEdge = true;
+  }
+  return containsEdge;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  //checks if edges does not contain toNode
+  if(this.storage[fromNode].edges.indexOf(this.storage[toNode]) === -1){
+    this.storage[fromNode].edges.push(this.storage[toNode]);
+    this.storage[toNode].edges.push(this.storage[fromNode]);
+  }
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  var fromIndex = this.storage[fromNode].edges.indexOf(this.storage[toNode]);
+  var toIndex = this.storage[toNode].edges.indexOf(this.storage[fromNode]);
+  if(index !== -1) {
+
+    this.storage[fromNode].edges.splice(fromIndex, 1);
+    this.storage[toNode].edges.splice(toIndex, 1);
+  }
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for (var key in this.storage) {
+    cb(key);
+  }
 };
+
+
+var GraphNode = function() {
+  // var obj = Object.create(this)
+  this.edges = [];
+
+  //return obj
+};
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
