15d32ad06ed332fc897e5a8769f48bfd85f03a79
josephlin55555 <josephlin55555@gmail.com>
2015-03-28 16:10:25 -0700

    completed hashTable

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,17 +1,42 @@
 var BinarySearchTree = function(value){
   //var this = Object.create(BinarySearchTree.prototype);
   this.storage = {};
+  this.storage.value = new BinaryTreeNode(value);
+  this.storage.head = this.storage.value;
+  //this.value = value;
   //return this;
+  debugger; //can't use this.storage; must refer to BST object
 };
 
-BinarySearchTree.prototype.insert = function (value) {
-  if (Object.keys(this.storage).length === 0) {
-    this.storage[value] = new BinaryTreeNode(value);
-    this.head = this.storage[value];
-  } else
-  {
 
-  }
+
+BinarySearchTree.prototype.insert = function (value) {
+  var checkNodes = function(node) {
+    var direction = undefined;
+    if(value < node.value && value !== node.value) {
+      direction = 'left';
+    } else {
+      direction = 'right';
+    }
+
+    if(node[direction] === undefined) {
+     node[direction] = new BinaryTreeNode(value);
+     node[direction];
+    } else {
+      checkNodes(node[direction]);
+    }
+    debugger;
+
+  };
+
+  // if (Object.keys(this.storage).length === 0) {
+  //   this.storage[value] = new BinaryTreeNode(value);
+  //   this.head = (this.storage)[value];
+  // }
+
+  checkNodes(this.storage.head);
+    //storage[value] = new BinaryTreeNode(value);
+
 };
 
 BinarySearchTree.prototype.contains = function () {
@@ -22,18 +47,13 @@ BinarySearchTree.prototype.depthFirstLog = function () {
 
 };
 
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
 var BinaryTreeNode = function (value) {
   this.value = value;
   this.left = undefined;
   this.right = undefined;
 };
 
+
 /*
-**********IMPORTANT!!!!!!
-Pseudoclassical style will not work due to tests not using new
-*/
+ * Complexity: What is the time complexity of the above functions?
+ */
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,74 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(i);
+
+  //if bucket doesn't exist, create new bucket
+  if(!bucket) {
+    bucket = [];
+    this._storage.set(i, bucket);
+  } //if bucket exists, check if key === k in tuple
+  var found = false;
+  for(var j = 0; j < bucket.length; j++) {
+    var tuple = bucket[j];
+    if(k === tuple[0]) {
+      tuple[1] = v;
+      found = true;
+    }
+  }
+
+  if(!found) {
+    bucket.push([k, v]);
+  }
+
+    //if yes, update key in tuple
+    //if not, add tuple to bucket
+
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+  // create variable to reference bucket at position i
+  var bucket = this._storage.get(i);
+
+  // if bucket does not exist, return null
+  if(!bucket) {
+    return null;
+  }
+
+  for(var j = 0; j < bucket.length; j++) {
+    var tuple = bucket[j];
+    // if tuple exists, return tuple[1]
+    if (tuple[0] === k) {
+      return tuple[1];
+    }
+  }
+
+  // if tuple does not exist, return null
+  return null;
+
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
 
+  var bucket = this._storage.get(i);
+
+  if(!bucket) {
+    return null;
+  }
+
+  for(var j = 0; j < bucket.length; j++) {
+    var tuple = bucket[j];
+    if (tuple[0] === k) {
+      bucket.splice(j, 1);
+      return tuple[1];
+    }
+  }
+  return null;
 };
 
 
@@ -21,3 +80,20 @@ HashTable.prototype.remove = function(k){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
